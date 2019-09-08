1f2f76e29f045fd3497f3b0d8eb2e08be7168896
jordan-cahill <jordan.cahill@us.sogeti.com>
2019-09-07 22:22:29 -0500

    iOS app skeleton

---
+++ BenchwarmersApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BenchwarmersApp
+//
+//  Created by Jordan Cahill on 9/7/19.
+//  Copyright © 2019 sogeti-benchwarmers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
