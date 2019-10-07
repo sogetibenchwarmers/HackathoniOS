3d7e26f2d2dae8da2eaf5c5c1a53d097ff7bae85
jordan-cahill <jordan.cahill@us.sogeti.com>
2019-10-06 19:49:33 -0500

    Add text fields to DeviceInfoController to display device data from the API

--- BenchwarmersApp/DeviceInfoController.swift
+++ BenchwarmersApp/DeviceInfoController.swift
@@ -12,11 +12,38 @@ class DeviceInfoController: UIViewController {
     var barcode: String?
     
     @IBOutlet weak var tfAssetTag: UITextField!
+    @IBOutlet weak var tfName: UITextField!
+    @IBOutlet weak var tfOwnedBy: UITextField!
+    @IBOutlet weak var tfStatus: UITextField!
+    @IBOutlet weak var tfSupportGroup: UITextField!
+    @IBOutlet weak var tfAssignmentGroup: UITextField!
+    @IBOutlet weak var tvSubLocation: UITextView!
+    @IBOutlet weak var tvLocation: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("device info controller loading")
+        
+        let fields = [
+            self.tfAssetTag!,
+            self.tfName!,
+            self.tfOwnedBy!,
+            self.tfStatus!,
+            self.tfSupportGroup!,
+            self.tfAssignmentGroup!,
+            self.tvSubLocation!,
+            self.tvLocation!
+        ]
+        
+        for field in fields {
+            field.layer.borderWidth = 2
+            field.layer.borderColor = UIColor(red: 0.98, green: 0.98, blue: 0.98, alpha: 1.0).cgColor
+            field.layer.cornerRadius = 5
+            field.clipsToBounds = true
+        }
+        
         tfAssetTag.text = self.barcode
+        
     }
     
 }
